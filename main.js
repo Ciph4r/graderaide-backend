eeba38e33375d9e568dd2b9853c48c933ebc3288
David Lau <David.lau@codeimmersives.com>
2020-04-15 16:25:29 -0400

    refactored to one line

--- main.js
+++ main.js
@@ -40,47 +40,46 @@ const Student = (name, course , term = 1) => {
     this.courses.push(Course(obj))
     },
     getAverage: function(crs,idx){
-      let allAverageGrade = []
-      const average = (array) => {
-        let total = 0
-        let count = 0
-        for (const num of array){
-          total += num.score
-          count ++
-        }
-        return total/count
+
+      if (this.courses.find(a => a.name === crs)){
+          if(idx !== undefined){
+        return  this.courses.find(a => a.name === crs).terms[idx].grades.reduce((a,b)=> a + b.score , 0) / this.courses.find(a => a.name === crs).terms[idx].grades.length
+      }else {
+        return  this.courses.find(a => a.name === crs).terms[this.term -1].grades.reduce((a,b)=> a + b.score , 0) / this.courses.find(a => a.name === crs).terms[this.term -1].grades.length
       }
-    for (const course of this.courses){
-      if (course.name === crs){
-        if (idx !== undefined){
-        return average(course.terms[idx].grades)
-        }else {
-          return average(course.terms[this.term-1].grades)
-      }
-        // {
-        //   for (const grade of course.terms){
-        //     console.log('grade')
-        //     console.log(grade)
-        //     allAverageGrade.push(average(grade.grades))
-          
-        //   }
-        //   return allAverageGrade.reduce((a,b) => a+b)/allAverageGrade.length
-        // }  
-
-    } else{ 
-      // return this.getAverage(this.courses)
-      for (const course of this.courses){
-        if (this.course === course.name){
-          return average(course.terms[this.term-1].grades)
-        }
-      }
-
+    }else{
+      return  this.courses.find(a => a.name === this.course).terms[this.term -1].grades.reduce((a,b)=> a + b.score , 0) / this.courses.find(a => a.name === this.course).terms[this.term -1].grades.length
     }
-    }
-  
+    
+    //   const average = (array) => {
+    //     let total = 0
+    //     let count = 0
+    //     for (const num of array){
+    //       total += num.score
+    //       count ++
+    //     }
+    //     return total/count
+    //   }
+    // for (const course of this.courses){
+    //   if (course.name === crs){
+    //     if (idx !== undefined){
+    //     return average(course.terms[idx].grades)
+    //     }else {
+    //       return average(course.terms[this.term-1].grades)
+    //   }
+    // } else{ 
+    //   // return this.getAverage(this.courses)
+    //   for (const course of this.courses){
+    //     if (this.course === course.name){
+    //       return average(course.terms[this.term-1].grades)
+    //     }
+    //   }
+      
 
-
+    // }
+    // }
     }
+
   }
   
 }
